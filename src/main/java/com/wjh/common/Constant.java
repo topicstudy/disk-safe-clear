f511ff674c7a6f4107e2e9b48748426a07c4e2b1
wjh <xxx@qq.com>
2021-10-04 18:01:53 +0800

    review

--- src/main/java/com/wjh/common/Constant.java
+++ src/main/java/com/wjh/common/Constant.java
@@ -1,7 +1,7 @@
 package com.wjh.common;
 
 public interface Constant {
-    boolean TEST_MODE = true;
+    boolean TEST_MODE = false;
     String TMP_FILE_SUFFIX = ".TMP";
     String LOGO_URI = "./src/main/resources/logo.png";
     String APP_NAME = "安全清理磁盘(不可恢复)";
